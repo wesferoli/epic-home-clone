0786f8636b99df8cb98ab1326b72070004846791
wesferoli <wesleyfer.oli97@gmail.com>
2022-01-24 01:04:13 -0300

    componente de input de busca finalizado

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,23 @@
+import '../../styles/components/Nav/Nav.sass'
+import SearchInput from '../SearchInput';
+
+const Nav = () => {
+  return (
+    <nav>
+      <SearchInput />
+
+      <div className='center-links'>
+        <span>Descobrir</span>
+        <span>Navegar</span>
+        <span>Novidades</span>
+      </div>
+
+      <div>
+        <span>Lista de desejos</span>
+        <span>Carrinho</span>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
