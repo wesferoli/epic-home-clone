15ef1cd84f2da42129bf992891f5d84a5ee3eac4
wesferoli <wesleyfer.oli97@gmail.com>
2022-01-24 01:10:18 -0300

    Página home com header e nav

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
 import Header from '../../components/Header'
+import Nav from '../../components/Nav';
 
 const Home = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <body style={{margin: '0 150px'}}>  
+        <Nav />
+      </body>
+    </>
+  )
 }
 
 export default Home;
